5559b9d3ed577bcca2fa8f68ef53c0d0d07ff140
Seon Wook Gim <tjsdnr960@naver.com>
2026-07-14 14:40:16 +0900

    Feat : 작물 생장 속성 관리를 위한 C++ 구조체 및 데이터테이블 세팅

---
+++ Source/MaumSeed/MaumCropData.h
@@ -0,0 +1,46 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "MaumCropData.generated.h"
+
+// 날씨 상태 정의
+UENUM(BlueprintType)
+enum class EMaumWeather : uint8
+{
+	Sunny UMETA(DisplayName = "맑음"),
+	Cloudy UMETA(DisplayName = "흐림"),
+	Rainy UMETA(DisplayName = "비")
+};
+
+// 작물 속성 정의를 위한 기반 구조체
+USTRUCT(BlueprintType)
+struct FMaumCropData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	public:
+	// 데이터 고유 식별자
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CropData")
+	FName CropId;
+
+	// UI 출력용 작물 이름
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CropData")
+	FString Name;
+
+	// 완숙까지 필요한 인게임 일수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CropData")
+	int32 GrowthDays;
+
+	// 하루 요구 물 주기 횟수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CropData")
+	int32 WaterPerDay;
+
+	// 성장 보너스를 받는 날씨
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CropData")
+	EMaumWeather PreferredWeather;
+
+	// 수확 시 기본 획득 점수
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "CropData")
+	int32 BaseScore;
+};
